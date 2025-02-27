736e9b71fcd5bf38b8a6a98b3c597148f1c663c9
Shivraj Roy <shivrajroy78@gmail.com>
2025-02-27 13:59:26 +0530

    feat: add localStorage support for storing selected places...

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,16 @@ function App() {
          const place = AVAILABLE_PLACES.find((place) => place.id === id);
          return [place, ...prevPickedPlaces];
       });
+
+      const storedIds =
+         JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+
+      if (storedIds.indexOf(id) === -1) {
+         localStorage.setItem(
+            "selectedPlaces",
+            JSON.stringify([id, ...storedIds])
+         );
+      }
    }
 
    function handleRemovePlace() {
@@ -47,6 +57,14 @@ function App() {
          prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
       );
       modal.current.close();
+
+      const storedIds =
+         JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+
+      localStorage.setItem(
+         "selectedPlaces",
+         JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+      );
    }
 
    return (
